4c7ec9bde580779022f5e615d2aa7de17a7b5d61
Alex Chang <achang209@gmail.com>
2020-01-04 20:31:34 -0600

    include logic that stores file in local file system

--- src/main/java/com/example/autoservicetrackerapi/controllers/InvoiceController.java
+++ src/main/java/com/example/autoservicetrackerapi/controllers/InvoiceController.java
@@ -2,12 +2,14 @@ package com.example.autoservicetrackerapi.controllers;
 
 import com.example.autoservicetrackerapi.models.Invoice;
 import com.example.autoservicetrackerapi.models.InvoiceDao;
+import com.example.autoservicetrackerapi.services.FileService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -17,13 +19,17 @@ public class InvoiceController {
     @Autowired
     private InvoiceDao invoiceDao;
 
+    @Autowired
+    private FileService fileService;
+
     @GetMapping("invoices")
     public List<Invoice> getInvoices () {
         return (List<Invoice>) invoiceDao.findAll();
     }
 
     @PostMapping("invoices")
-    public void addInvoice(@RequestParam String invoice, @RequestParam MultipartFile file) throws JsonProcessingException {
+    public void addInvoice(@RequestParam String invoice, @RequestParam MultipartFile file) throws IOException {
+        fileService.uploadFile(file);
         Invoice invoiceObj = new ObjectMapper().readValue(invoice, Invoice.class);
         invoiceObj.setFilePath(file.getOriginalFilename());
         invoiceDao.save(invoiceObj);
