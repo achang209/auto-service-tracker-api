12e6efb861e168770b4f91f88250cb170c75c36d
Alex Chang <achang209@gmail.com>
2020-01-17 12:43:26 -0600

    create method that queries db by service performed

--- src/main/java/com/example/autoservicetrackerapi/controllers/SearchController.java
+++ src/main/java/com/example/autoservicetrackerapi/controllers/SearchController.java
@@ -3,28 +3,39 @@ package com.example.autoservicetrackerapi.controllers;
 import com.example.autoservicetrackerapi.models.Invoice;
 import com.example.autoservicetrackerapi.models.InvoiceDao;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("search")
 @CrossOrigin(origins = "http://localhost:4200")
 public class SearchController {
 
     @Autowired
     private InvoiceDao invoiceDao;
 
-    @GetMapping("search")
+    @GetMapping("provider")
     public List<Invoice> searchByProvider(@RequestParam String provider) {
         Iterable<Invoice> allInvoices = invoiceDao.findAll();
         List<Invoice> invoicesToSend = new ArrayList<>();
 
         for (Invoice invoice : allInvoices) {
             if (invoice.getServiceProvider().toLowerCase().contains(provider)) {
+                invoicesToSend.add(invoice);
+            }
+        }
+        return invoicesToSend;
+    }
+
+    @GetMapping("service")
+    public List<Invoice> searchByService(@RequestParam String service) {
+        Iterable<Invoice> allInvoices = invoiceDao.findAll();
+        List<Invoice> invoicesToSend = new ArrayList<>();
+
+        for (Invoice invoice : allInvoices) {
+            if (invoice.getServicePerformed().toLowerCase().contains(service)) {
                 invoicesToSend.add(invoice);
             }
         }
