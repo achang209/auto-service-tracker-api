d614ed14c15554914bf3ff4c71727044d438e326
Alex Chang <achang209@gmail.com>
2020-01-21 18:33:05 -0600

    refactor get method to utilize various ui and data facing model classes

--- src/main/java/com/example/autoservicetrackerapi/controllers/ServiceProviderController.java
+++ src/main/java/com/example/autoservicetrackerapi/controllers/ServiceProviderController.java
@@ -1,12 +1,19 @@
 package com.example.autoservicetrackerapi.controllers;
 
+import com.example.autoservicetrackerapi.models.InvoiceDto;
 import com.example.autoservicetrackerapi.models.ServiceProvider;
 import com.example.autoservicetrackerapi.models.ServiceProviderDao;
+import com.example.autoservicetrackerapi.models.ServiceProviderDto;
+import com.example.autoservicetrackerapi.models.ui.InvoiceDetailsResponse;
+import com.example.autoservicetrackerapi.models.ui.ServiceProviderResponse;
+import com.example.autoservicetrackerapi.services.ProviderServiceImpl;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -14,10 +21,18 @@ import java.util.List;
 public class ServiceProviderController {
 
     @Autowired
-    private ServiceProviderDao serviceProviderDao;
+    private ProviderServiceImpl providerService;
 
     @GetMapping("service_provider")
-    public List<ServiceProvider> getServiceProviders () {
-        return (List<ServiceProvider>) serviceProviderDao.findAll();
+    public List<ServiceProviderResponse> getServiceProviders () {
+        List<ServiceProviderDto> providers = providerService.getServiceProviders();
+        List<ServiceProviderResponse> returnValue = new ArrayList<>();
+
+        for (ServiceProviderDto serviceProviderDto : providers) {
+            ServiceProviderResponse serviceProviderResponse = new ServiceProviderResponse();
+            BeanUtils.copyProperties(serviceProviderDto, serviceProviderResponse);
+            returnValue.add(serviceProviderResponse);
+        }
+        return returnValue;
     }
 }
