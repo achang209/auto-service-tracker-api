7ae6a318c188c8c126bed7c19f57a748cb0e3f48
Alex Chang <achang209@gmail.com>
2020-01-22 21:52:54 -0600

    rename controllers to match entity names

--- src/main/java/com/example/autoservicetrackerapi/controllers/RepairController.java
+++ src/main/java/com/example/autoservicetrackerapi/controllers/RepairController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
-public class ServicePerformedController {
+public class RepairController {
 
     @Autowired
     private ServicePerformedServiceImpl servicePerformedService;
--- src/main/java/com/example/autoservicetrackerapi/controllers/VendorController.java
+++ src/main/java/com/example/autoservicetrackerapi/controllers/VendorController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
-public class ServiceProviderController {
+public class VendorController {
 
     @Autowired
     private VendorServiceImpl providerService;
