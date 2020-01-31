ab6ef0af60637f772e15493d09063f7bf2f4a338
Alex Chang <achang209@gmail.com>
2020-01-31 17:49:39 -0600

    add validation annotations to InvoiceDto

--- src/main/java/com/example/autoservicetrackerapi/model/InvoiceDto.java
+++ src/main/java/com/example/autoservicetrackerapi/model/InvoiceDto.java
@@ -2,13 +2,17 @@ package com.example.autoservicetrackerapi.model;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
 
 public class InvoiceDto implements Serializable {
     private static final long serialVersionUID = 6460692248095615980L;
 
     private int id;
+    @NotBlank(message = "Must enter date.")
     private String date;
+    @Min(value = 1, message = "Must enter mileage that is greater than 0")
     private int mileage;
     private int vendorId;
     private Vendor vendor;
