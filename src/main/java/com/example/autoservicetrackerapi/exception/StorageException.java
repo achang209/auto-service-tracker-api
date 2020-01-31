1f234f11ea5729dc7747a3fd1f6408b30181269a
Alex Chang <achang209@gmail.com>
2020-01-31 17:55:28 -0600

    auto generate serialVersionUID property

--- src/main/java/com/example/autoservicetrackerapi/exception/StorageException.java
+++ src/main/java/com/example/autoservicetrackerapi/exception/StorageException.java
@@ -1,6 +1,8 @@
 package com.example.autoservicetrackerapi.exception;
 
 public class StorageException extends RuntimeException {
+    private static final long serialVersionUID = 6366453013044211665L;
+
     public StorageException(String message) {
         super(message);
     }
--- src/main/java/com/example/autoservicetrackerapi/exception/StorageFileNotFoundException.java
+++ src/main/java/com/example/autoservicetrackerapi/exception/StorageFileNotFoundException.java
@@ -2,6 +2,8 @@ package com.example.autoservicetrackerapi.exception;
 
 public class StorageFileNotFoundException extends StorageException {
 
+    private static final long serialVersionUID = 6873613153457043228L;
+
     public StorageFileNotFoundException(String message) {
         super(message);
     }
